19a554437668fabaa655244c330eccf0a5c2a28e
gabi emi <gabiemi2021@gmail.com>
2026-05-18 20:15:01 -0300

    adiciona api do grupo do tupinamba e fernando

--- src/config/apis.js
+++ src/config/apis.js
@@ -4,15 +4,20 @@ export const APIs = [
         url: "https://clubelivro-backend.onrender.com/api/livros",
         apiKey: import.meta.env.VITE_API_KEY_ENTRE_LINHAS,
     },
-        {
+    {
+        origem: 'dev-stone',
+        url: "https://clubelivro-backend-zui4.onrender.com/api/livro",
+        apiKey: import.meta.env.VITE_API_KEY_DEV_STONE,
+    },
+    {
         origem: 'menino-grande',
         url: "https://atividade-portugues-backend.onrender.com/api/livro",
         apiKey: import.meta.env.VITE_API_KEY_MENINO_GRANDE,
     },
-        {
-        origem: 'dev-stone',
-        url: "https://clubelivro-backend-zui4.onrender.com/api/livro",
-        apiKey: import.meta.env.VITE_API_KEY_DEV_STONE,
+    {
+        origem: 'olhos-agua',
+        url: "https://olhosdagua.onrender.com/api/livro",
+        apiKey: import.meta.env.VITE_API_KEY_OLHOS_AGUA,
     },
 ];
 
