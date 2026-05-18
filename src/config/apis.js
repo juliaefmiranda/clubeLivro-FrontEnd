0750b5d7435c8e89747b8d2c79045b6504f4dcf2
gabi emi <gabiemi2021@gmail.com>
2026-05-18 19:16:32 -0300

    faz integracao com outras apis

---
+++ src/config/apis.js
@@ -0,0 +1,18 @@
+export const APIs = [
+    {
+        origem: 'minha-api',
+        url: "https://clubelivro-backend.onrender.com/api/livros",
+        apiKey: import.meta.env.VITE_API_KEY_ENTRE_LINHAS,
+    },
+        {
+        origem: 'menino-grande',
+        url: "https://atividade-portugues-backend.onrender.com/api/livro",
+        apiKey: import.meta.env.VITE_API_KEY_MENINO_GRANDE,
+    },
+        {
+        origem: 'dev-stone',
+        url: "https://clubelivro-backend-zui4.onrender.com/api/livro",
+        apiKey: import.meta.env.VITE_API_KEY_DEV_STONE,
+    },
+];
+
